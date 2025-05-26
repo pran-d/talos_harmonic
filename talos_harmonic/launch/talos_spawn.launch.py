3fdb6c65126ee6765f10d9eda0f7577dda195000
Pranav Debbad <pdebbad@ontake>
2025-05-26 16:16:02 +0200

    WIP: trying to setup ros2_control

--- talos_harmonic/launch/talos_spawn.launch.py
+++ talos_harmonic/launch/talos_spawn.launch.py
@@ -15,15 +15,16 @@ from itertools import (
 )
 
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, TimerAction
+from launch.actions import SetEnvironmentVariable, ExecuteProcess, TimerAction
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from ros_gz_bridge.actions import RosGzBridge
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-
+        
     model = PathJoinSubstitution([
         FindPackageShare('talos_harmonic'),
         'models', 'talos', 'empty_talos_gz.sdf'
@@ -33,18 +34,40 @@ def generate_launch_description():
         FindPackageShare('talos_harmonic'),
         'launch', 'controller_bridge.yaml'
     ])
+
+    urdf_file = os.path.join(
+        get_package_share_directory('talos_harmonic'),
+        'models',
+        'talos',
+        'talos_reduced.urdf'
+    )
+
+    with open(urdf_file, 'r') as infp:
+        robot_description = infp.read()
    
     return LaunchDescription([
+
+        SetEnvironmentVariable(name='GZ_SIM_RESOURCE_PATH', value='/home/pdebbad/ros_space/talos_harmonic/src/talos_harmonic/models'),
+
+        SetEnvironmentVariable(name='GZ_SIM_SYSTEM_PLUGIN_PATH', value='/opt/ros/jazzy/lib'),
 
         ExecuteProcess(
             cmd=['gz', 'sim', '--physics-engine', 'gz-physics7-bullet-featherstone-plugin', '-r', model],
             output='screen'
         ),
 
-        RosGzBridge(
-            bridge_name="controller_bridge",
-            config_file=config,
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
         ),
+
+        # RosGzBridge(
+        #     bridge_name="controller_bridge",
+        #     config_file=config,
+        # ),
 
         # Node(
         #     package='robot_state_publisher',
